d283033c04fefe2881ae2e96b4942a61a0af3830
Zawar5 <116018146+Zawar5@users.noreply.github.com>
2024-01-08 13:50:58 +0000

    changes done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+let accountEmail = "muhammadzawar2@gmail.com"
+var accountPassword = "1234"
+
+accountCity = "Lahore"
+
+// accountId = 2
+
+accountEmail = "xyz@gmail.com"
+
+accountPassword = "121212"
+
+accountCity = "Faisalabad"
+
+console.table([accountEmail, accountPassword, accountCity])
+
+// console.log(accountId);
+
+
+
