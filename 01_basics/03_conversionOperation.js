b1f46bad590aaf703281b5cb4793276600126442
Zawar5 <116018146+Zawar5@users.noreply.github.com>
2024-01-09 07:42:19 +0000

    Conversion uwpdate

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+// const {score} = req.body
+
+// console.log(typeof (score));
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+
+
+let isLoggedIn = 1
+
+let booleanIsLogged = Boolean(isLoggedIn);
+
+
+
+
