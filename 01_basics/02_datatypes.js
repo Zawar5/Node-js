a819ef5aa31b1d8036f969cc13dc38ff99eeefb2
Zawar5 <116018146+Zawar5@users.noreply.github.com>
2024-01-09 07:22:56 +0000

    Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,34 @@
+"use strict" // treat all js code as newer version.
+
+// JavaScript Standards define via ECMA Scrip (https://tc39.es/ecma262) and MDN.
+// MDN (https://developer.mozilla.org/en-US/) is the thirdparty Web doc and we will use this documentation most.
+
+//alert (3+3) // We are using node js not browser
+
+console.log(3 
+    + 3); // This is not a good practive to add extra linespaces. The code readability is the High priority
+
+console.log("Hello Node");
 
 
+let name = "Zawar"
+let age = 28 
+let isLoggedIn = true
 
+// Number => 2 to the power of either 52 or 53
+// Bigint => for big number specially for trading or big data numbers
+
+// Sting => "Hello"
+
+// Boolean => True or False
+
+// Null => Standalone value and alos a datatype/ representation of empty variable
+
+// undefined => value is not defined
+
+// Symbol => find the uniqueness
+
+// Objects => 
+
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
