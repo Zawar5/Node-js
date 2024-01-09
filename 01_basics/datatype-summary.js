b4300ab0bf1b58d5cdfcc6835e82136ad0a22aba
muhammad <muhammadzawar2@gmail.com>
2024-01-09 17:52:38 +0500

    Memory Allocation

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,36 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+/*****************  Memory Allocation  ******************************/
+
+// Stack (Primitive type), Heap (Non Primitive or Reference type i.e Array, Object, function)
+
+let myName = "Muhamamad"
+
+let otherName = myName
+otherName = "Zawar"
+
+console.log(myName);
+console.log(otherName);
+
+let userInfo = {
+    userEmail : "xyz@gmail.com",
+    userId : "id@xyz"
+}
+
+// console.log(userInfo);
+
+let userTwo = userInfo;
+
+// console.log(userTwo);
+
+userTwo.userEmail = "abc@yahoo.com"
+
+console.log(userInfo.userEmail);
+console.log(userTwo.userEmail);
