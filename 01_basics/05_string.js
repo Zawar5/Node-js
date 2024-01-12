e3f435fd95efc2e665391e8b36cb68c5ff733f32
muhammad <muhammadzawar2@gmail.com>
2024-01-12 17:02:13 +0500

    Staring 05

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = "Zawar"
+
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+// console.log(`My name is ${name} and I've ${repoCount} Repo counts`);
+
+const gameName = new String("New-String")
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString =  gameName.slice(-8, 6)
+// console.log(anotherString);
+
+const newStringOne = "    Test    "
+
+// console.log(newStringOne);
+
+// console.log(newStringOne.trim());
+
+const url = "https://zawar.com/test%20page"
+// console.log(url);
+
+// console.log(url.replace('%20', '-' ));
+
+// console.log(url.includes('zawar'));
+
+console.log(gameName.split('-'));
