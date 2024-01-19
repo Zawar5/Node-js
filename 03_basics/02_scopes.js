4dd671394e0b06d1fbf76636809d27bc7b6f8b80
muhammad <muhammadzawar2@gmail.com>
2024-01-19 16:54:12 +0500

    Scopes details

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,6 +8,49 @@ const b = 20
 var c = 30
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+// ***************  Nested Scope ******************************
+
+
+function one(){
+    const username = "Arsh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
+
+
+
+
+if(true){
+    const username = "Arshman"
+
+    if(username === "Arshman"){
+        const website = " Facebook"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+function addOne(num){
+    return num + 1
+}
+addOne(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
