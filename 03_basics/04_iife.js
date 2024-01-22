ce1557440785308f8382db1ceaf43133272f9221
muhammad <muhammadzawar2@gmail.com>
2024-01-22 14:52:30 +0500

    IIFE functions

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,12 @@
 // Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // Named iife
+    console.log(`DB CONNECTED`);
+})();
+// chai()
+
+// Arrow Function IIFE
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('Arsh')
